fe2c0d3eaadd5bb8c11356f7aca1f8c5a17ab48e
yunslee <yunafrica@gmail.com>
2021-02-11 17:09:34 +0900

    로드밸런서로 external-ip에 minikube ip가 설정되긴했으나, 실제 nginx에 접근하지는 못하는 상태

--- del_minikube_dashboard.sh
+++ del_minikube_dashboard.sh
@@ -1,5 +1,21 @@
 echo "\033[33m";echo "----------delete.sh----------";echo "\033[0m"
+
+kubectl delete -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+
+kubectl delete services --all
+echo "\033[33m"kubectl delete services --all"\033[0m"
+
+kubectl delete deploy --all
+echo "\033[33m"kubectl delete deploy --all"\033[0m"
+# kubectl delete persistentvolume --all
+
+echo "\033[33m"kubectl delete configmap --all"\033[0m"
+kubectl delete configmap --all
+
+echo "\033[33m"kubectl delete po --all"\033[0m"
+kubectl delete po --all
+
 echo "\033[33m"minikube stop"\033[0m"
 minikube stop
-echo "\033[33m"minikube delete"\033[0m"
+# echo "\033[33m"minikube delete"\033[0m"
 # minikube delete
--- mak_minikube_dashboard.sh
+++ mak_minikube_dashboard.sh
@@ -2,9 +2,20 @@ echo "\033[33m";echo "----------setup.sh----------";echo "\033[0m"
 echo "\033[33m";echo "minikube start --driver=virtualbox";echo "\033[0m"
 minikube start --driver=virtualbox
 
-echo "\033[33m";echo "minikube addons enable metallb";echo "\033[0m"
-minikube addons enable metallb
+# echo "\033[33m";echo "minikube addons enable metallb";echo "\033[0m"
+# minikube addons enable metallb
 # kubectl apply -f
+
+echo "\033[33m";echo '$(minikube docker-env)';echo "\033[0m"
+eval $(minikube docker-env)
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+kubectl create -f ./srcs/metallb/metallb-configmap.yaml
+
+
+docker build -t nginx ./srcs/nginx/
+kubectl create -f ./srcs/nginx/nginx-service.yaml
 echo "\033[33m";echo "minikube dashboard";echo "\033[0m"
 minikube dashboard
 echo "\033[33m";echo "Finish metalLB setup";echo "\033[0m"
